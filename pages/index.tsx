378c5cb6c8475557464c0af916655efb6ecaf631
Cydiater <cydiater@gmail.com>
2022-01-21 01:39:41 +0800

    add init message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
 	return (
@@ -8,9 +8,16 @@ const Home: NextPage = () => {
 				<title>Cydiater Site</title>
 				<meta name="description" content="Personal Site of Cydiater" />
 			</Head>
-			<h1 className="text-3xl font-bold text-center text-sky-800 pt-10">
-				Do not go gentle into that good night.
-			</h1>
+			<div className="flex flex-col items-center space-y-2 pt-5">
+				<h1 className="text-3xl font-bold text-center text-stone-700">
+					Hi 👋 , I&#39;m Cydiater
+				</h1>
+
+				<p className="text-neutral-400 italic max-w-xl">
+					This is a trivial side-project for building a personal blog website to help me kill the spare holiday time. 
+					Checkout the <a href="https://github.com/Cydiater/Web" className="underline">Github Repo</a> to view the source code and reach me out.
+				</p>
+			</div>
 		</div>
 	)
 }
