83ec3153b9d445a31a367c533f00ef9ebe7d2754
Cydiater <cydiater@gmail.com>
2022-01-24 14:11:02 +0800

    add component PostPreview

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,45 +1,26 @@
 import { formatISO } from 'date-fns';
-import { FunctionComponent } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 
-type PostPreviewProps = {
-	id: string,
-	title: string,
-	postDate: Date,
-	intro: string,
-	imagePath: string,
-};
-
-export const PostPreview: FunctionComponent<PostPreviewProps> = ({
-	id,
+export default function PostPreview( {
 	title,
 	postDate,
 	intro,
-	imagePath,
-}) => {
+} : {
+	title: string,
+	postDate: Date,
+	intro: string,
+}) {
 	return (
-		<div className="flex sm:flex-row flex-col w-full px-2">
-			<div className="text-neutral-400 text-base whitespace-nowrap mr-5">
+		<div className="flex flex-row space-x-6 w-full">
+			<div className="text-neutral-400 text-base">
 				{formatISO(postDate, {
 					representation: "date",
 				})}
 			</div>
 			<div className="flex flex-col space-y-1">
-				<Link href={`/posts/${id}`} passHref>
-					<button className="text-xl font-bold font-serif hover:text-blue-600 text-left transition transition-all ease-in-out">
-						<a>{title}</a>
-					</button>
-				</Link>
-				<div className="w-full h-60 relative">
-					<Image 
-						src={imagePath}
-						layout="fill"
-						objectFit="contain"
-						alt="main image of the post"
-					/>
+				<div className="text-xl font-bold">
+					{title}
 				</div>
-				<div className="text-neutral-500 text-md">
+				<div className="text-neutral-500 text-lg">
 					{intro}
 				</div>
 			</div>
