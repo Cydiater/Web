bf840441e880d7765dfe62dd32c903bcbe4a5146
Cydiater <cydiater@gmail.com>
2022-01-25 01:08:11 +0800

    add dynamic route for posts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,39 +1,8 @@
-import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
-import { getAllPostIds, getPostData, PostData } from '../../lib/posts'
-import { ParsedUrlQuery } from 'querystring';
-import { PostLayout } from '../../components/post-layout';
+import { useRouter } from 'next/router';
 
-type Props = {
-	postData: PostData
-};
+export const Post = () => {
+	const router = useRouter()
+	const { id } = router.query
 
-interface Params extends ParsedUrlQuery {
-	id: string,
+	return <p>Post: {id}</p>
 }
-
-export const getStaticPaths: GetStaticPaths = async () => {
-	const paths = getAllPostIds()
-	return {
-		paths,
-		fallback: false
-	}
-}
-
-
-export const getStaticProps: GetStaticProps<Props, Params> = async (ctx) => {
-	const params = ctx.params!;
-	const postData = await getPostData(params.id)
-	return {
-		props: {
-			postData
-		}
-	};
-}
-
-const PostView: NextPage<Props> = ({ postData }) => {
-	return (
-		<PostLayout {...postData} />
-	);
-}
-
-export default PostView;
