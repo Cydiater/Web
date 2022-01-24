3999c0940ef64fe12802f65856d12b26771d59df
Cydiater <cydiater@gmail.com>
2022-01-24 14:11:02 +0800

    add component PostPreview

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,33 +1,25 @@
 import { formatISO } from 'date-fns';
-import { FunctionComponent } from 'react';
-import Link from 'next/link';
 
-type PostPreviewProps = {
-	id: string,
-	title: string,
-	postDate: Date,
-	intro: string,
-};
-
-export const PostPreview: FunctionComponent<PostPreviewProps> = ({
-	id,
+export default function PostPreview( {
 	title,
 	postDate,
 	intro,
-}) => {
+} : {
+	title: string,
+	postDate: Date,
+	intro: string,
+}) {
 	return (
-		<div className="flex flex-row space-x-6 w-full px-2">
-			<div className="text-neutral-400 text-base whitespace-nowrap">
+		<div className="flex flex-row space-x-6 w-full">
+			<div className="text-neutral-400 text-base">
 				{formatISO(postDate, {
 					representation: "date",
 				})}
 			</div>
 			<div className="flex flex-col space-y-1">
-				<Link href={`/posts/${id}`} passHref>
-					<button className="text-xl font-bold font-serif hover:text-blue-600 text-left transition transition-all ease-in-out">
-						<a>{title}</a>
-					</button>
-				</Link>
+				<div className="text-xl font-bold">
+					{title}
+				</div>
 				<div className="text-neutral-500 text-lg">
 					{intro}
 				</div>
