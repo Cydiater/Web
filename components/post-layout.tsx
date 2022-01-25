2acb4cf67b6051c3bee1008407c1cd12de3425bc
Cydiater <cydiater@gmail.com>
2022-01-25 17:41:17 +0800

    add date and back button

--- components/post-layout.tsx
+++ components/post-layout.tsx
@@ -1,12 +1,25 @@
 import type { PostData } from '../lib/posts';
+import Link from 'next/link';
 import { FunctionComponent } from 'react';
 
 export const PostLayout: FunctionComponent<PostData> = (postData) => {
 	return (
 		<div className="flex flex-col items-center">
 			<div className="flex flex-col max-w-2xl w-full px-3 pt-3">
+				<div className="flex flex-row text-sm text-neutral-400 pb-2">
+					<Link href='/'>
+						Back
+					</Link>
+					<div className="grow"/>
+					<div>
+						{postData.postDateString}
+					</div>
+				</div>
 				<div className="text-2xl font-bold font-serif"> 
 					{postData.title} 
+				</div>
+				<div className="text-neutral-500 pb-5">
+					{postData.intro}
 				</div>
 				<div 
 					className="prose leading-normal"
